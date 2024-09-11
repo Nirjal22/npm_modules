ce513088d1ba914bb68413ec39781394ab9f2b16
Nirjal22 <bistanirjal978@gmail.com>
2024-09-11 11:03:40 +0545

    js code

---
+++ dateAndTime/index.js
@@ -0,0 +1,7 @@
+var format = require('date-format');
+
+// Create a new Date object
+var date = new Date();
+
+// Format the date in the desired format, for example 'yyyy' for the year
+console.log(format('yyyy'-'dd'-'MM', date));
